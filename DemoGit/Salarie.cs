ad7cd672d090b80ac0264161c6f6707d874bb75e
GG <gg@2isa.com>
2016-04-14 11:22:02 +0200

    Ajout d'une sous-classe de Personne : Salarie

---
+++ DemoGit/Salarie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DemoGit
+{
+    public class Salarie : Personne
+    {
+        private int _Salaire;
+
+        public int Salaire
+        {
+            get
+            {
+                return _Salaire;
+            }
+
+            set
+            {
+                _Salaire = value;
+            }
+        }
+
+        public Salarie(string nom,int age, int salaire) : base (nom, age)
+        {
+            this.Salaire = salaire;
+        }
+
+    }
+}
